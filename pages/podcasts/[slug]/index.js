9b980c8cceff78cc17fe54de9deaebba853ed663
Patrick Morris <patrickjohnmorris@gmail.com>
2022-02-09 18:30:31 -0500

    smaller slug

--- pages/podcasts/[slug]/index.js
+++ pages/podcasts/[slug]/index.js
@@ -1,13 +1,12 @@
 import { FEEDS, getFeed, getFeedEpisodes } from "@/lib/podcast";
 import { format } from "date-fns";
 import BlurImage from "@/components/BlurImage";
-import selectProps from "@/lib/selectProps";
 import { Anchorme } from 'react-anchorme';
 import Link from "next/link";
-import {slugify} from "@/lib/util"
+import {slugify} from "@/lib/util";
 
 
-export default function Feed({ items, itunes, feed, title, description }) {
+export default function Feed({ items, itunes, feed, description, lastBuildDate }) {
   return (
     <div className="max-w-xl px-6 py-12 mx-auto">
     <Link href='/podcasts'><a>See more Podcasts</a></Link>
@@ -15,36 +14,40 @@ export default function Feed({ items, itunes, feed, title, description }) {
         <h1 className="mb-12 text-5xl font-bold">{feed.title}</h1>
         <BlurImage 
             priority="true" 
-            src={itunes.image ?? items.itunes.image}
+            src={itunes.image ?? items.image}
             width={500}
             height={500}
             layout="responsive"
             objectFit="cover"
             />
             <Anchorme>{description}</Anchorme>
-     {/* <p>Last Updated: {format(new Date(lastBuildDate), "PPP")}</p> */}
+     {lastBuildDate ? <p>Last Updated: {format(new Date(lastBuildDate), "PPP")}</p>: ''}
      </div>     
       <div className="space-y-4">
         {items.map((item) => (
+          <div key={item.guid}>
           <Link
-            key={item.guid}
             className="block p-4 border border-gray-200 rounded-lg hover:border-gray-500"
             href={`/podcasts/${feed.slug}/${slugify(item.title)}`}
           >
             <a>
             <div className="font-bold">{item.title}</div>
             <BlurImage  
-              src={item.itunes.image ? item.itunes.image : itunes.image}  
-              alt={item.itunes.name ? item.itunes.name : item.title}
+              src={item.image ?? itunes.image}  
+              alt={item.title}
               width={250}
               height={250}
               layout="responsive"
               objectFit="cover"
             />
-            {item.contentSnippet && <p className="truncate">{item.contentSnippet}</p>}
-            <div>{format(new Date(item.isoDate), "PPP")}</div>
             </a>
           </Link>
+          {
+              item.description ? <p className="">{item.description}</p> :
+              item.contentSnippet ? <p className="">{item.contentSnippet}</p> : ''
+            }
+            <div>{format(new Date(item.isoDate), "PPP")}</div>
+            </div>
         ))}
       </div>
     </div>
@@ -52,10 +55,6 @@ export default function Feed({ items, itunes, feed, title, description }) {
 }
 
 export async function getStaticPaths() {
-    const titleEpisodes = await getFeedEpisodes()
-    // const notUndefined = anyValue => typeof anyValue !== 'undefined'
-    // console.log(titleEpisodes)
-    // const data = titleEpisodes.map((u, i) => ({params: {slug: slugify([u.allFeeds[i].podcastTitle]) }}))
     const data = FEEDS.map((podcast) => ({
         params: { slug: podcast.slug}
       }))
@@ -68,15 +67,23 @@ export async function getStaticPaths() {
 export async function getStaticProps({ params }) {
   const feed = FEEDS.find((feed) => feed.slug === params.slug);
   const detailedFeed = await getFeed(feed.url);
-  // const itemsForIndex = detailedFeed.items.map(selectProps("guid", "title", "isoDate", "itunes", "contentSnippet"))
+  const itemsForIndex = detailedFeed.items.map((item) => ({
+    guid: item.guid,
+    title: item.title,
+    isoDate: item.isoDate,
+    image: item.itunes.image || null,
+    description: item.description || null,
+    contentSnippet: item.contentSnippet || null,
+  }))
+  
   return {
     props: {
       feed,
-      items: detailedFeed.items,
-      // items: itemsForIndex,
+      items: itemsForIndex,
       itunes: detailedFeed.itunes,
       title: detailedFeed.title,
       description: detailedFeed.description,
+      author: detailedFeed.itunes.author ?? detailedFeed.creator
     },
     revalidate: 1,
   };

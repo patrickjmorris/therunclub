b642e555302c044afc3de1916a6b787d64f9fcea
Patrick Morris <patrickjohnmorris@gmail.com>
2024-10-04 11:57:54 -0400

    wrap description on episode page

--- src/app/(main)/podcasts/[podcast]/[episode]/page.tsx
+++ src/app/(main)/podcasts/[podcast]/[episode]/page.tsx
@@ -116,7 +116,7 @@ export default async function Episode({
 				</header>
 				<hr className="my-12 border-gray-200" />
 				<div
-					className="prose prose-slate mt-14 [&>h2:nth-of-type(3n)]:before:bg-violet-200 [&>h2:nth-of-type(3n+2)]:before:bg-indigo-200 [&>h2]:mt-12 [&>h2]:flex [&>h2]:items-center [&>h2]:font-mono [&>h2]:text-sm [&>h2]:font-medium [&>h2]:leading-7 [&>h2]:text-slate-900 [&>h2]:before:mr-3 [&>h2]:before:h-3 [&>h2]:before:w-1.5 [&>h2]:before:rounded-r-full [&>h2]:before:bg-cyan-200 [&>ul]:mt-6 [&>ul]:list-['\2013\20'] [&>ul]:pl-5"
+					className="prose prose-slate whitespace-pre-wrap mt-14 [&>h2:nth-of-type(3n)]:before:bg-violet-200 [&>h2:nth-of-type(3n+2)]:before:bg-indigo-200 [&>h2]:mt-12 [&>h2]:flex [&>h2]:items-center [&>h2]:font-mono [&>h2]:text-sm [&>h2]:font-medium [&>h2]:leading-7 [&>h2]:text-slate-900 [&>h2]:before:mr-3 [&>h2]:before:h-3 [&>h2]:before:w-1.5 [&>h2]:before:rounded-r-full [&>h2]:before:bg-cyan-200 [&>ul]:mt-6 [&>ul]:list-['\2013\20'] [&>ul]:pl-5"
 					// biome-ignore lint/security/noDangerouslySetInnerHtml: We want to use the HTML content to apply styling
 					dangerouslySetInnerHTML={{ __html: episode.description }}
 				/>

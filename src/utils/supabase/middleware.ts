c97de83ab601efdfc4ca34372d7da87f2993eb1d
Patrick Morris <patrickjohnmorris@gmail.com>
2024-11-05 12:42:09 -0500

    edit middleware to enable all pages

--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -47,7 +47,8 @@ export async function updateSession(request: NextRequest) {
 		// no user, potentially respond by redirecting the user to the login page
 		const url = request.nextUrl.clone();
 		url.pathname = "/login";
-		return NextResponse.redirect(url);
+
+		// return NextResponse.redirect(url);
 	}
 
 	// IMPORTANT: You *must* return the supabaseResponse object as it is. If you're

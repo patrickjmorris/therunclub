0d8b92b1be25e9eb716c62cc9c5c321d6821e353
Patrick Morris <patrickjohnmorris@gmail.com>
2024-11-16 15:09:02 -0500

    update import

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -1,18 +1,18 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import { Provider as JotaiProvider } from "jotai"
-import { ThemeProvider as NextThemesProvider } from "next-themes"
-import { ThemeProviderProps } from "next-themes/dist/types"
+import * as React from "react";
+import { Provider as JotaiProvider } from "jotai";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { ThemeProviderProps } from "next-themes";
 
-import { TooltipProvider } from "@/components/ui/tooltip"
+import { TooltipProvider } from "@/components/ui/tooltip";
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return (
-    <JotaiProvider>
-      <NextThemesProvider {...props}>
-        <TooltipProvider delayDuration={0}>{children}</TooltipProvider>
-      </NextThemesProvider>
-    </JotaiProvider>
-  )
+	return (
+		<JotaiProvider>
+			<NextThemesProvider {...props}>
+				<TooltipProvider delayDuration={0}>{children}</TooltipProvider>
+			</NextThemesProvider>
+		</JotaiProvider>
+	);
 }

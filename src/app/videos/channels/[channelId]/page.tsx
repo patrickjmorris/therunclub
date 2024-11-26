11fca1351e9de0a3354f6ab1f768c4a0a464ca4b
Patrick Morris <patrickjohnmorris@gmail.com>
2024-11-26 16:37:00 -0500

    more type errors

--- src/app/videos/channels/[channelId]/page.tsx
+++ src/app/videos/channels/[channelId]/page.tsx
@@ -21,7 +21,7 @@ interface ChannelPageProps {
 export async function generateStaticParams() {
 	// Get first 5 channels from database ordered by subscriber count
 	const channels = await getAllChannels();
-	return channels.slice(0, 5).map((channel) => ({
+	return channels.map((channel) => ({
 		channelId: channel.id,
 	}));
 }
